cc1c5597e8b5cec2a31a3d12800938dcf9cde546
Shreyash Mandlik <shreyashmandlik38@gmail.com>
2025-08-01 17:39:47 +0530

    fist commit on newBranch

--- first.java
+++ first.java
@@ -2,5 +2,6 @@ public class first{
     public static void main(String[] args){
         System.out.println("Hello from the first file!");
         System.out.println("calling second file's main method:");
+        System.out.println("executing seconfd file");
     } 
 }
