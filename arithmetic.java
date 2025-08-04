ad8bffcb34c2671f77d5cbb2c67c73382c610feb
Shreyash Mandlik <shreyashmandlik38@gmail.com>
2025-08-04 19:03:16 +0530

    Arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of A:");
+        int a= sc.nextInt();
+        System.out.println("enter the value of B:");
+        int b = sc.nextInt();
+        System.out.println("result:");
+        int add= a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println("result is:");
+        
+        
+        
+    }
+    
+    
+    
+    
+}
